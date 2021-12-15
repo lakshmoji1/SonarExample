cf09e250422a47598785efa1f56388652117e2b4
lakshmoji1 <srilakshmoji@gmail.com>
2021-12-15 13:09:31 +0530

    1st commit in master

--- src/main/kotlin/org/example/Hello.kt
+++ src/main/kotlin/org/example/Hello.kt
@@ -18,6 +18,11 @@ class Hello
         println("hello everyone hi")
     }
 
+    fun bymaster()
+    {
+        println("added in master branch")
+    }
+
 }
 /*
 Added a branch named LakshmojiBranch
